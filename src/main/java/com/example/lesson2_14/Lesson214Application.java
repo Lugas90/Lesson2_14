717a55da9c29a822da7d1e5e56248e31c653c4b7
Lugas90 <Lugas90@mail.ru>
2022-08-03 07:57:41 +0300

    1 Комит

---
+++ src/main/java/com/example/lesson2_14/Lesson214Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson2_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson214Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson214Application.class, args);
+    }
+
+}
